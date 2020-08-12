179511e28bd7fb1eb1bf4091131ae472945863e8
Kamali Means <darenkmeans@gmail.com>
2020-08-12 13:20:55 -0700

    update render sequence - last commit before big function refactor

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function generateSequence() {
     sequences.randomSequence.push(randomNum());
     sequences.playerSequence = [];
     counter = 0;
-    setTimeout(renderSeq, 1000)
+    renderSeq();
 };
 function init() {
     gameOver = false;
@@ -89,8 +89,6 @@ function handleClick(evt) {
     if(gameOver || ignoreClicks) {
         return; 
     } else if (evt.target === buttonEls.greenButton) {
-        // buttonEls.greenButton.style.borderBottomColor='rgba(0, 183, 0, 1)';
-          //this works to change color of button!
          sequences.playerSequence.push(0);
          isGameOver();
     } else if (evt.target === buttonEls.redButton) {
@@ -123,8 +121,10 @@ console.log(gameOver)
 };
 
 function renderSeq() {
-
-}
+    let lightShow =sequences.randomSequence;
+    console.log('is this lightshow?', lightShow)
+    if (lightShow.[index] == 0) lightUp.green;
+};
 /* sketch out function order here */
 
 function turnLightsOff(){
